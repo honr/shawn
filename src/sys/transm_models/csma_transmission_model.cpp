9f26938f7313d5fccb320f0074b4a5698ef1e323
Markus <huellebrand@users.sourceforge.net>
2007-09-14 13:05:23 +0000

    git-svn-id: https://shawn.svn.sourceforge.net/svnroot/shawn@68 3422d224-1e22-0410-b8a2-c109bc27950b

---
+++ src/sys/transm_models/csma_transmission_model.cpp
@@ -0,0 +1,245 @@
+/************************************************************************
+** This file is part of the network simulator Shawn.                  **
+** Copyright (C) 2004-2007 by the SwarmNet (www.swarmnet.de) project  **
+** Shawn is free software; you can redistribute it and/or modify it   **
+** under the terms of the BSD License. Refer to the shawn-licence.txt **
+** file in the root of the Shawn source tree for further details.     **
+************************************************************************/
+
+#include "sys/transm_models/csma_transmission_model.h"
+#include "sys/edge_model.h"
+#include "sys/world.h"
+#include "sys/simulation/simulation_controller.h"
+#include "sys/simulation/simulation_environment.h"
+
+namespace shawn
+	{
+	CsmaTransmissionModel::
+		CsmaTransmissionModel(int bandwidth, double backoff, double sending_jitter) :
+	received_(0), 
+		dropped_(0), 
+		packet_failure_(0), 
+		average_delay_(0), 
+		jitter_(0), 
+		deliver_num_(0),
+		backOff_(backoff),
+		bandwidth_(bandwidth),
+		sending_jitter_(sending_jitter)
+		{
+		}
+
+	// ----------------------------------------------------------------------
+	CsmaTransmissionModel::
+		~CsmaTransmissionModel()
+		{}
+
+	// ----------------------------------------------------------------------
+	void CsmaTransmissionModel::init()
+		throw()
+		{
+		TransmissionModel::init();
+		if( neighbors_ != NULL )
+			delete neighbors_;
+		//This gives us an Array of Messages for all nodes which can be received in O(1)
+		neighbors_ = new DynamicNodeArray<MessageList>(world_w());
+		}
+
+	// ----------------------------------------------------------------------
+	void CsmaTransmissionModel::reset()
+		throw()
+		{
+		average_delay_ = deliver_num_>0? average_delay_/deliver_num_ : 0;
+		jitter_ = deliver_num_>1? jitter_/(deliver_num_-1) :0;
+		std::cout << "CSMA: "<<  
+			received_ <<" msgs. to be sent. "<< dropped_ << " msgs. dropped." 
+			<< packet_failure_ << " packets fail to reach the destination." << " And average delay is "
+			<< average_delay_  <<". Jitter is "<<jitter_<< std::endl;
+
+		TransmissionModel::reset();
+		received_ = 0;
+		dropped_ = 0;
+		packet_failure_ = 0;
+		average_delay_ = 0;
+		deliver_num_ = 0;
+		jitter_ = 0;
+		}
+	// ----------------------------------------------------------------------
+
+
+	// ----------------------------------------------------------------------
+	bool
+		CsmaTransmissionModel::
+		supports_mobility( void )
+		const throw(std::logic_error)
+		{
+		return world().edge_model().supports_mobility();
+		}
+
+	// ----------------------------------------------------------------------
+	void CsmaTransmissionModel::send_message( MessageInfo &mi )
+		throw()
+		{
+		csma_msg* new_msg = new csma_msg(&mi, mi.msg_->size() / bandwidth_, backOff_);
+		// If sending jitter is set, deliver time will be changed. ( Needed to avoid same deliver times due to 
+		// processors sending at the beginning of a round.)
+		mi.time_=( sending_jitter_ > 0.0 ) ? ( new_msg->random( 0.001, sending_jitter_ ) + mi.time_) : ( mi.time_ );
+		new_msg->deliver_time = mi.time_;
+		find_destinations( new_msg );
+		//Adds a message to the DynamicNodeArray
+		MessageList* m = &((*neighbors_)[*(mi.src_)]);
+		m->insert(new_msg);
+		// Create a new Event. Important is the MessageTag (new_msg). It defines which message will be send
+		event_handle_ = world_w().scheduler_w().new_event(*this, mi.time_,new_msg);
+		}
+
+	void CsmaTransmissionModel::timeout(EventScheduler & event_scheduler, EventScheduler::EventHandle event_handle, double time, EventScheduler::EventTagHandle & event_tag_handle) throw()
+		{
+		csma_msg* msg = dynamic_cast<csma_msg* >(event_tag_handle.get());
+		if(msg != NULL){
+			if(!msg->sending)
+				deliver(msg); // If the message has not been send yet.
+			else    //Otherwise the transmission has terminated
+				(*neighbors_)[*(msg->pmi->src_)].erase(msg);
+
+			}
+		else
+			std::cout<< "msg = NULL"<<std::endl;
+
+		}
+
+	// ----------------------------------------------------------------------
+	void CsmaTransmissionModel::
+		deliver_messages() throw(){}
+
+	void CsmaTransmissionModel::
+		deliver(csma_msg* msg) throw(){
+			//Checks if medium is free
+			listening(msg);
+			//When medium is free msg->isSending() otherwise not
+			if(msg->isSending()){
+				Node* source = msg->pmi->src_;
+				deliver_num_++;
+				std::cout <<"csma: "<< "Source Node: " <<  source->id() 
+					<< ". Msg. send time: " <<  msg->deliver_time
+					<< ". Msg. deliver time: " <<  msg->pmi->time_
+					<< std::endl;
+				for(unsigned int i=0; i< msg->destinations_.size(); i++){
+					//All receiving nodes will check, if they already receive a message 
+					receive(msg->destinations_[i]->dest_node_,msg);
+					}
+				//Define a new Event for the end of the transmision. (Specified by msg->isSending())
+				event_handle_= world_w().scheduler_w().new_event(*this, msg->deliver_time + msg->duration_,msg);
+				}
+		}
+
+	void CsmaTransmissionModel::
+		listening(csma_msg* msg) throw(){
+
+			double nextFreeTime=world().current_time();
+			bool sending = false;
+			//All neighbors are checked here
+			EdgeModel::adjacency_iterator it, endit;
+			for( it = world_w().begin_adjacent_nodes_w( *msg->pmi->src_ ),
+				endit = world_w().end_adjacent_nodes_w( *msg->pmi->src_ );
+				it != endit;
+			++it )
+				{
+				if(msg->pmi->src_->id() ==it->id()) continue;
+				MessageList* m = &((*neighbors_)[*it]);
+				MessageList::iterator iter = m->begin();
+				//Each neighbors messages are tested
+				for(; iter!=m->end();++iter){
+					csma_msg* temp = *iter;
+					if(msg->deliver_time > temp->deliver_time && msg->deliver_time <= temp->deliver_time + temp->duration_)
+						{
+						std::cout <<"csma: "<< "Node " << temp->pmi->src_->id() << " is interfering" << std::endl;
+						double time_fin = temp->deliver_time + temp->duration_;
+						sending = true;
+						nextFreeTime = (nextFreeTime>= time_fin)? (nextFreeTime):(time_fin);
+						}
+// Case when to messages are send at the same time is dealt with at the receiving node.
+					}
+				}
+			if(!sending){
+				//If no neighbor is sending a message this message will be send
+				msg->setSending();
+				}
+			else{
+				std::cout<<"csma: "<<"Other node sending. Message from node: "<<msg->pmi->src_->id() <<" delayed"<< std::endl;
+				msg->deliver_time= nextFreeTime + msg->backoff;
+				//New Event. To the next time the medium is free + a backoff
+				event_handle_ = world_w().scheduler_w().new_event(*this,msg->deliver_time,msg);
+				}
+		}
+
+	void CsmaTransmissionModel::
+		receive(Node* target, csma_msg* msg) throw(){
+			bool inUse=false;
+			EdgeModel::adjacency_iterator it, endit;
+			// The target checks if one of it's neighbors (others than the msgs. one) is already sending
+			for( it = world_w().begin_adjacent_nodes_w( *target),
+				endit = world_w().end_adjacent_nodes_w( *target );
+				it != endit;
+			++it )
+				{
+				if(msg->pmi->src_->id() != it->id()){
+					MessageList* m = &((*neighbors_)[*it]);
+					MessageList::iterator iter = m->begin();
+					//Here all messages of one node is checked
+					for(; iter!=m->end();++iter){
+						// First conditionckecks if a node not adjacent to the sender is delivering a msg. to target at the moment 
+						// as well as if sender and receiver are delivering at the same moment.
+						// This procedure correlates with reality. Both nodes are sending and cannot "hear" the other one
+						if((msg->deliver_time >= (*iter)->deliver_time && msg->deliver_time <= (*iter)->deliver_time + (*iter)->duration_) )
+							{
+							inUse = true;
+							break;
+							}					
+						}
+					if(inUse) break;
+					}
+				}
+
+			if(inUse){
+				std::cout<<"csma: "<<"Message from Node:"<< msg->pmi->src_->id() << "dropped. Due to conflict at node:"
+					<< target->id()<< std::endl;
+				++dropped_;
+
+				}
+			else{
+				std::cout<<"csma: "<<"Node:"<< target->id() << "received msg. from node:"
+					<< msg->pmi->src_->id()<< std::endl;
+				++received_;
+				double cur_delay=(msg->deliver_time-msg->pmi->time_);
+				average_delay_ +=cur_delay; 
+				if( deliver_num_>1 )
+					jitter_ += (cur_delay>last_delay_)? cur_delay-last_delay_ : last_delay_-cur_delay;
+				last_delay_ = cur_delay;
+				target->receive(ConstMessageHandle(msg->pmi->msg_));
+				}
+
+		}
+
+	// ----------------------------------------------------------------------
+	void CsmaTransmissionModel::
+		find_destinations( csma_msg* pmsg )
+		{
+		EdgeModel::adjacency_iterator it, endit;
+		for( it = world_w().begin_adjacent_nodes_w( *pmsg->pmi->src_ ),
+			endit = world_w().end_adjacent_nodes_w( *pmsg->pmi->src_ );
+			it != endit;
+		++it )
+			{
+			if(pmsg->pmi->src_->id()!= it->id())
+				pmsg->push_new_destination( &(*it) );
+			}
+		}
+
+	}
+/*-----------------------------------------------------------------------
+* Source  $Source: /cvs/shawn/shawn/sys/transm_models/csma_transmission_model.cpp,v $
+* Version $Revision: 38 $
+* Date    $Date: 2007-06-08 14:30:12 +0200 (Fr, 08 Jun 2007) $
+*-----------------------------------------------------------------------
+* $Log: csma_transmission_model.cpp,v $
+*-----------------------------------------------------------------------*/
---
+++ src/sys/transm_models/csma_transmission_model.h
@@ -0,0 +1,171 @@
+/************************************************************************
+ ** This file is part of the network simulator Shawn.                  **
+ ** Copyright (C) 2004-2007 by the SwarmNet (www.swarmnet.de) project  **
+ ** Shawn is free software; you can redistribute it and/or modify it   **
+ ** under the terms of the BSD License. Refer to the shawn-licence.txt **
+ ** file in the root of the Shawn source tree for further details.     **
+ ************************************************************************/
+#ifndef __SHAWN_SYS_TRANS_MODELS_CSMA_H
+#define __SHAWN_SYS_TRANS_MODELS_CSMA_H
+
+#include "sys/transmission_model.h"
+#include "sys/event_scheduler.h"
+#include <sys/misc/dynamic_node_array.h>
+#include "sys/transm_models/csma_transmission_model_message.h"
+#include <set>
+
+namespace shawn
+{
+	/**
+	 *@brief A CsmaTransmissionmodel senses the channel before it delivers a message.
+	 * This class implements the CSMA/CA transmission model.
+	 * A processor waits for the medium to be free before sending. 
+	 * If a neighbor is already sending, the new message will be delayed until the end of
+	 * the transmission plus a backoff time
+	 * In this implementation one can define parameters for the mediums bandwidth and the upper bound for 
+	 * the randomy chosen backoff
+	 * This module can be called by:
+	 * transm_model=csma bandwidth=1 backoff=0.5    
+	 */
+	class CsmaTransmissionModel : public TransmissionModel,EventScheduler::EventHandler
+	{
+	public:
+		///@name construction, destruction and support for life cycle
+		///@{
+		CsmaTransmissionModel(int bandwidth, double backoff, double sending_jitter);
+		~CsmaTransmissionModel();
+		/**
+		 *@brief Initialize the csma transmission model
+		 *
+		 * Gets the list of neighbors
+		 */
+		virtual void init() throw();
+		/**
+		 *@brief Reset the csma transmission model
+		 *
+		 *Clear the statistic variables and set the parameters to its default value.
+		 */
+		virtual void reset() throw();
+		///@}
+
+		///@name Transmission Model Implementation
+		///@{
+		/**
+		 *@brief Mobility is depending on mobility support from the edge model
+		 *
+		 *The edge model is used to determine the 1-hop neighbours 
+         *which will receive the message
+		 */
+
+		virtual bool supports_mobility( void ) const throw(std::logic_error);
+		
+		/**
+		 *@brief Stores each message in a vector for delivery at the next simulation round start
+		 *
+		 *A new structure of csma_msg will be build and inserted into aired_messages_ 
+		 *@see aired_messages_
+		 */
+        virtual void send_message( MessageInfo& mi ) throw();
+
+		  ///@name basic method inherited from EventHandler 
+		  ///@{
+		  /**
+			* Timeout Event. Receives a csma_msg as a EventScheduler::EventTagHandle.
+			* If message has not been sent yet, deliver() will be called.
+			* Otherwise message has been transfered and will be deleted from sending processor's MessageList
+		  */
+		  virtual void CsmaTransmissionModel::timeout(EventScheduler & event_scheduler, EventScheduler::EventHandle event_handle, double time, EventScheduler::EventTagHandle & event_tag_handle) throw();
+		  ///@}
+
+		 /**
+		 *@brief Delivers all messages which are in the vector
+		 * Method inherited from shawn::TransmissionModel. Is not implemented in this module since the messages are 
+		 * added to the shawn::EventScheduler and do not have to be delivered at the beginning of a round.
+		 */ 
+        virtual void deliver_messages() throw();
+		///@}
+
+		///@name Delivers a message.
+		/**
+		* This method implements the CSMA/CA algorithm.
+		* The processor listens if a neighbor is sending. 
+		* If no neighbor is sending, it will deliver its message, otherwise
+		* it will delay the message to the end of the transmission plus a random backoff.
+		*@param msg Pointer to the message to be send
+		*/
+		void deliver(csma_msg* msg) throw();
+		///}
+		private:
+		///@name Method which tests if medium is free
+		///@{
+		/**
+		* This method tests if any neighbor is sending a message.
+		* If medium is free, message is set to sending otherwise not.
+		*@param msg Pointer to the message to be send
+		*/
+		void listening(csma_msg* msg) throw();
+		///}
+		///@name Method in which a given node will receive a message
+		///@{
+		/**
+		* This method delivers a message to a given node.
+		* It has to check that the target is not receiving a message from a node which is not adjacent to the sender.
+		* In this case the message will be dropped.
+		*@param target Pointer to the target node
+		*@param msg Pointer to the message to be send
+		*/
+		void receive(Node* target, csma_msg* msg) throw();
+		///@}
+
+		/**
+		 *@brief Find the neighbors of the node who send a message
+		 *
+		 *Find all the destinations of a new-coming message, which are the neighbours of its source node
+		 *@param pmsg the message whose source node's neighbors to be determined
+		 */
+		void find_destinations( csma_msg* pmsg );
+
+		///Number of received messages
+		int received_;
+
+		///Number of messages dropped
+		int dropped_;
+
+		// The number of transmission failure
+		int packet_failure_;
+
+		///Number of packets failed to reach the destination
+		double average_delay_;
+
+		///The average variation of delay
+		double jitter_;
+
+		///Number of messages delivered so far
+		int deliver_num_;
+
+		///The delay of last message
+		double last_delay_;
+
+		///Backoff that will be waited after a message has been delayed
+		double backOff_;
+		///Bandwidth, defines the throughput
+		int bandwidth_;
+		/// EventHandle
+		EventScheduler::EventHandle event_handle_;
+		/// Jitter for starting the transmission
+		double sending_jitter_;
+
+		///The messages that have been sent by the nodes and are waiting for delivery
+		typedef std::set<csma_msg*> MessageList;
+		/// List of nodes in transmission range.
+		DynamicNodeArray<MessageList>*  neighbors_;
+	};
+}
+#endif
+/*-----------------------------------------------------------------------
+ * Source  $Source: /cvs/shawn/shawn/sys/transm_models/csma_transmission_model.h,v $
+ * Version $Revision: 38 $
+ * Date    $Date: 2007-06-08 14:30:12 +0200 (Fr, 08 Jun 2007) $
+ *-----------------------------------------------------------------------
+ * $Log: csma_transmission_model.h,v $
+ *-----------------------------------------------------------------------*/
