fc105499ef94e28823ded95265ce48ef4950753b
CSinger <csinger08@users.sourceforge.net>
2009-02-11 14:18:50 +0000

    Configuration options shifted to CMakeLists.txt
    git-svn-id: https://shawn.svn.sourceforge.net/svnroot/shawn@323 3422d224-1e22-0410-b8a2-c109bc27950b

--- src/apps/vis/module.cmake
+++ src/apps/vis/module.cmake
@@ -2,52 +2,6 @@
 # Shawn module configuration for cmake build system
 #=============
 
-    OPTION(OPT_ENABLE_CAIRO "Enable Cairo library support" OFF)
-
-    if ( OPT_ENABLE_CAIRO )
-      set ( DEFAULT_CAIRO_ROOT )
-      set ( LIB_PATH_CAIRO   CACHE PATH "Path to CAIRO library" )
-      set ( INCLUDE_PATH_CAIRO   CACHE PATH "Path to CAIRO includes" )
-	link_directories( ${LIB_PATH_CAIRO} )
-      include_directories ( ${INCLUDE_PATH_CAIRO} )
-      
-      add_definitions( -DHAVE_CAIRO )
-    endif ( OPT_ENABLE_CAIRO )
-
-    OPTION(OPT_ENABLE_BOOST "Enable boost library support" OFF)
-    if( OPT_ENABLE_BOOST )
-      set ( LIB_PATH_BOOST   CACHE PATH "Boost lib path" )
-      set ( INCLUDE_PATH_BOOST   CACHE PATH "Boost include path" )
-      link_directories( ${LIB_PATH_BOOST} )
-      include_directories ( ${INCLUDE_PATH_BOOST} )
-      add_definitions(-DHAVE_BOOST -DHAVE_BOOST_REGEX )
-    endif( OPT_ENABLE_BOOST )
-
-    OPTION(OPT_ENABLE_GLUT "Enable GLUT library used by Vis Liveview")
-    if ( OPT_ENABLE_GLUT )
-       add_definitions( -DHAVE_GLUT )
-    endif ( OPT_ENABLE_GLUT )
-
-    if (  WIN32 AND NOT CYGWIN  )
-        if ( OPT_ENABLE_CAIRO )
-            if ( LIB_PATH_CAIRO )
-		set ( SHAWN_LINK_LIBS ${SHAWN_LINK_LIBS} cairo.lib )
-            endif ( LIB_PATH_CAIRO )
-        endif ( OPT_ENABLE_CAIRO )
-    else (  WIN32 AND NOT CYGWIN  )
-        if ( OPT_ENABLE_CAIRO )
-            if ( LIB_PATH_CAIRO )
-				    # $SHAWN_LINK_LIBS is used in src/CMakeLists.txt at
-				    # the end of the file when calling
-				    # "target_link_libraries". It can't be used here,
-				    # because "add_executable" must be called first.
-					 set ( SHAWN_LINK_LIBS ${SHAWN_LINK_LIBS} cairo pixman-1 png freetype fontconfig z 
-						 	boost_regex boost_date_time boost_thread GL glut )
-#boost_regex-gcc-mt )
-            endif ( LIB_PATH_CAIRO )
-        endif ( OPT_ENABLE_CAIRO )
-    endif (  WIN32 AND NOT CYGWIN  )
-
 # Name of this module
 
 	set ( moduleName VIS )
