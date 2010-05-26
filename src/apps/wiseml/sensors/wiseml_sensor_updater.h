201e2d676c0ac1d03f034bf578b25964ac937cb8
CSinger <csinger08@users.sourceforge.net>
2010-05-26 16:42:11 +0000

    WiseML capability support
    git-svn-id: https://shawn.svn.sourceforge.net/svnroot/shawn@420 3422d224-1e22-0410-b8a2-c109bc27950b

---
+++ src/apps/wiseml/sensors/wiseml_sensor_updater.h
@@ -0,0 +1,45 @@
+/************************************************************************
+ ** This file is part of the network simulator Shawn.                  **
+ ** Copyright (C) 2004-2007 by the SwarmNet (www.swarmnet.de) project  **
+ ** Shawn is free software; you can redistribute it and/or modify it   **
+ ** under the terms of the BSD License. Refer to the shawn-licence.txt **
+ ** file in the root of the Shawn source tree for further details.     **
+ ************************************************************************/
+#ifndef __SHAWN_APPS_WISEML_SENSOR_UPDATER_H
+#define __SHAWN_APPS_WISEML_SENSOR_UPDATER_H
+#include "_apps_enable_cmake.h"
+#ifdef ENABLE_WISEML
+#include "apps/wiseml/sensors/wiseml_sensor.h"
+#include "apps/wiseml/sensors/wiseml_raw_sensor.h"
+#include "apps/reading/sensors/sensor_keeper.h"
+#include "sys/simulation/simulation_controller.h"
+ 
+using namespace shawn;
+using namespace std;
+namespace wiseml
+{
+   /** \brief Updates attached sensors
+    */
+   class WisemlSensorUpdater
+   {
+   public:
+      WisemlSensorUpdater();
+      virtual ~WisemlSensorUpdater();
+      
+      void register_updater(SimulationController &sc);
+
+      void remove_sensor( string sensor_id );
+      void update_sensor( string sensor_id, string value );
+
+      WisemlRawSensor* operator[](string sensor_id);
+
+      bool sensor_exists(string sensor_id);
+	   map<string, WisemlRawSensor*> reg_sensors_;
+
+   protected:
+      typedef map<string, RefcntPointer<reading::SensorFactory>>::iterator SFIterator;
+   };
+}
+
+#endif
+#endif
