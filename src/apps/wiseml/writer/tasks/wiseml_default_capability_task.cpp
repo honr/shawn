9e35f8c37e21035d3affa961e81434a85fe3fb2b
CSinger <csinger08@users.sourceforge.net>
2010-06-24 09:42:51 +0000

    WiseML Tasks
    git-svn-id: https://shawn.svn.sourceforge.net/svnroot/shawn@444 3422d224-1e22-0410-b8a2-c109bc27950b

---
+++ src/apps/wiseml/writer/tasks/wiseml_default_capability_task.cpp
@@ -0,0 +1,52 @@
+/************************************************************************
+ ** This file is part of the network simulator Shawn.                  **
+ ** Copyright (C) 2004-2010 by the SwarmNet (www.swarmnet.de) project  **
+ ** Shawn is free software; you can redistribute it and/or modify it   **
+ ** under the terms of the BSD License. Refer to the shawn-licence.txt **
+ ** file in the root of the Shawn source tree for further details.     **
+ ************************************************************************/
+#include "apps/wiseml/writer/tasks/wiseml_default_capability_task.h"
+#ifdef ENABLE_WISEML
+#include <iostream>
+#include <fstream>
+#include "apps/wiseml/writer/wiseml_generator.h"
+#include "apps/wiseml/writer/wiseml_data_keeper.h"
+namespace wiseml
+{
+   WisemlDefaultCapabilityTask::WisemlDefaultCapabilityTask()
+   {
+   }
+   // ----------------------------------------------------------------------
+   WisemlDefaultCapabilityTask::~WisemlDefaultCapabilityTask()
+   {
+   }
+   // ----------------------------------------------------------------------
+   // ----------------------------------------------------------------------
+   std::string WisemlDefaultCapabilityTask::name() const throw()
+   {
+      return "wiseml_default_capability";
+   }
+   // ----------------------------------------------------------------------
+   std::string WisemlDefaultCapabilityTask::description() const throw()
+   {
+      return "Gathers data for the setup section of a WiseML file.";
+   }
+   // ----------------------------------------------------------------------
+   // ----------------------------------------------------------------------
+   void WisemlDefaultCapabilityTask::run(SimulationController &sc) throw()
+   {
+      WisemlDataKeeper *keeper = 
+         sc.keeper_by_name_w<WisemlDataKeeper>("wiseml_data_keeper");
+      WisemlSetupCollector &setup = keeper->setup();
+
+      Capability cap;
+      cap.name = sc.environment().required_string_param("name");
+      cap.datatype = sc.environment().required_string_param("datatype");
+      cap.unit = sc.environment().required_string_param("unit");
+      cap.def_value = sc.environment().required_string_param("value");
+
+      setup.set_default_node_capability(cap);
+
+   }
+}
+#endif
